3ad24200a4fe499fb8eadb79c1d6f4db77166029
par1si <pparisi13@gmail.com>
2020-04-14 14:56:42 -0700

    added menus & version

---
+++ cmds/help.js
@@ -0,0 +1,20 @@
+const menus = {
+    main: `
+    optimizelyCLI [command] <options>
+
+    hello world ..................... log "Hello World" to the console.
+    `,
+    create: `
+    optimizelyCLI [command] <options>
+
+    createFeature ................... creates a new Optimizely feature for the specified project option.
+    `
+}
+
+module.exports = (args) => {
+    const subCmd = args._[0] === 'help'
+    ? args._[1]
+    : args._[2]
+
+    console.log(menus[subCmd] || menus.main)
+}
--- index.js
+++ index.js
@@ -5,13 +5,30 @@ const minimist = require('minimist');
 
 module.exports = () => {
     const args = minimist(process.argv.slice(2))
-    const cmd = args._[0]
+    let cmd = args._[0] || 'help'
+
+    if (args.version || args.v) {
+        cmd = 'version'
+    }
+
+    if (args.help || args.h) {
+        cmd = 'help'
+    }
 
 
     switch(cmd) {
         case 'hello world':
             require('./cmds/hello_world')(args)
             break
+
+        case 'version':
+            require('./cmds/version')(args)
+            break
+
+        case 'help':
+            require('./cmds/help')(args)
+            break
+            
         default:
             console.log(`"${cmd}" is not a valid command. Please check your spelling and try again.`)
             break
