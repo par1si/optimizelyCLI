df9e41de001f681adcee26895d474ce2d61b2342
par1si <pparisi13@gmail.com>
2020-04-14 14:33:52 -0700

    added a cmds directory & first cmd script

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const minimist = require('minimist');
 
 module.exports = () => {
     const args = minimist(process.argv.slice(2))
-    console.log(args)
+    const cmd = args._[0]
+
+
+    switch(cmd) {
+        case 'hello world':
+            require('./cmds/hello_world')(args)
+            break
+        default:
+            console.log(`"${cmd}" is not a valid command. Please check your spelling and try again.`)
+            break
+    }
 }
 
 // Try running the following commands from the root directory:
 // sudo npm link
-// optimizelyCLI hello
-// optimizelyCLI --createProject "Sample Project"
+// optimizelyCLI "hello world"
