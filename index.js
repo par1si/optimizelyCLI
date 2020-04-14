83e1c310a6b081ab6fed041d7d2f03a6c5e4a3a5
par1si <pparisi13@gmail.com>
2020-04-14 14:17:27 -0700

    added minimist & sample commands to index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Minimist is a library that parses CLI arguments.
+const minimist = require('minimist');
+
+
+
 module.exports = () => {
-    console.log('Welcome to Optimizely CLI!')
+    const args = minimist(process.argv.slice(2))
+    console.log(args)
 }
+
+// Try running the following commands from the root directory:
+// sudo npm link
+// optimizelyCLI hello
+// optimizelyCLI --createProject "Sample Project"
