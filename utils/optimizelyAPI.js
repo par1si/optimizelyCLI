cf128c7f29881f50e751e884c8b75377b6f826b6
par1si <pparisi13@gmail.com>
2020-04-15 09:28:32 -0700

    added support for all CRUD operations

--- utils/optimizelyAPI.js
+++ utils/optimizelyAPI.js
@@ -2,13 +2,14 @@ const axios = require('axios')
 require('dotenv').config()
 
 
-module.exports = async function getData (endpoint) {
+module.exports = async function getData (endpoint, method) {
   let url =`https://api.optimizely.com/v2/${endpoint}`
-  const results = await axios.get(url, { // replaced with Optimizely specific URL with ${endpoint} specified
+  const results = await axios({
+    method: method,
+    url,
     headers: {
       Authorization: `Bearer ${process.env.OPTLY_API_KEY}`
     }
   })
   return results
 }
-
