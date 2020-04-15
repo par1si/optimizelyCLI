5de7787a1700a6a2318eed2e926ec78d02befef3
par1si <pparisi13@gmail.com>
2020-04-15 00:10:00 -0700

    fixed bug with api key

--- cmds/register.js
+++ cmds/register.js
@@ -2,12 +2,13 @@ const fs = require("fs");
 require('dotenv').config()
 
 module.exports = (args) => {
-    const apiKey = args.a
+    const apiKey = args._[1]
 
+    // This only creates a file - if a .env file already exists, this will not update it.
     fs.writeFile(".env", `OPTLY_API_KEY="${apiKey}"`, function(err) {
         if(err) {
             return console.log(err);
         }
-        console.log(`Optimizely API Key registered with the value "${apiKey}.`);
+        console.log(`Optimizely API Key registered with the value ${apiKey}.`);
     }); 
 } 
