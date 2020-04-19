e11631c818b875dd0993f7fb75173f32581bf299
par1si <pparisi13@gmail.com>
2020-04-18 18:27:02 -0700

    added ability to get data

--- cmds/get.js
+++ cmds/get.js
@@ -4,11 +4,17 @@ module.exports = (args) => {
     let endpoint
 
     // Parser when endpoint is entered incorrectly.
-    if (args.c === "project" || "p" || "projects") {
+    if (args.g === "project" || "p" || "projects") {
         endpoint = "projects"
     }
 
-    optimizelyAPI(`${endpoint}`, 'get', undefined)
+    // Here's how you would add params for a project ID (for like experiments)
+    let params = args.id
+    // Sample command: optimizelyCLI -g experiments -id 8392480986
+    // should return experiments running in the project ID 8392480986 as long as we pass in ?project_id=8392480986 at the
+    // end of the URL
+
+    optimizelyAPI(`${endpoint}`, 'get')
 } 
 
 // Sample command: optimizelyCLI -g "projects"
